b62547bda774e2f2b7b361157c1b37b57187ebc2
Guilherme Selair <guilhermeselair@gmail.com>
2022-10-27 14:22:14 -0300

    Feat: Added Eslint, StyledComponent, Theme and Global Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
   return (
-   <h1>Guilherme Selair</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Guilherme Selair</h1>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
